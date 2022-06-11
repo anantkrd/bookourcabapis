9a6be78d66872529f17613c13482795731cdfb77
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-06-12 00:55:57 +0530

    adding0

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const pool=createPool({
     user:process.env.DB_USER,
     host:process.env.HOST,
     database:process.env.MYSQL_DB,
-    password:process.env.PASS,
+    password:process.env.DB_PASS,
     connectionLimit:10
 });
 module.exports=pool;
