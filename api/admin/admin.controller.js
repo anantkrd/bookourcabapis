4874e029208fd53aa146dcc79882e09c60bcec7a
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-06-12 18:15:34 +0530

    api cahnges

--- api/admin/admin.controller.js
+++ api/admin/admin.controller.js
@@ -1,5 +1,5 @@
 const { json } = require('body-parser');
-const{getBookingsAdminHome,updateAgentAmount,getBookingsForAgent,getCompletedBookings}=require('./admin.service');
+const{getBookingsAdminHome,updateAgentAmount,getBookingsForAgent,getCompletedBookings,getReadyBooking}=require('./admin.service');
 module.exports={
     getBookingsAdminHome:async(pageId)=>{   
         let data=await getBookingsAdminHome(pageId); 
@@ -25,6 +25,18 @@ module.exports={
     getCompletedBookings:async(agentId,pageId=1)=>{
         //console.log("Here in controleler");
         let results=await getCompletedBookings(pageId); 
+         //console.log("datares*=="+JSON.stringify(datares));
+         if(results.length<=0){
+            responce=JSON.stringify({code:'500',msg:'No Data found',data:''});
+        }else{
+            
+            responce=JSON.stringify({code:'200',msg:'',data:results});
+        }
+         return responce;        
+    },
+    getReadyBooking:async(agentId,pageId=1)=>{
+        //console.log("Here in controleler");
+        let results=await getReadyBooking(pageId); 
          //console.log("datares*=="+JSON.stringify(datares));
          if(results.length<=0){
             responce=JSON.stringify({code:'500',msg:'No Data found',data:''});
