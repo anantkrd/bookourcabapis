e636a8d4f0d6ae81954893a70a211ed34418fd85
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-06-17 22:49:09 +0530

    add prfile

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -52,7 +52,7 @@ module.exports={
         });
     },
     getUserByID:async (userId)=>{
-        sqlcheck="select id,firstName,lastName,mobileNo,email,userType,createdTime from prayag_users where id=?";
+        sqlcheck="select id,firstName,lastName,mobileNo,email,userType,createdTime,idProof,idNumber,profileImage from prayag_users where id=?";
         console.log("select id,firstName,lastName,mobileNo,email,userType,idProof,idNumber,profileImage,createdTime from prayag_users where id="+userId)
         return new Promise((resolve, reject)=>{
             pool.query(sqlcheck,[userId],  (error, results)=>{
