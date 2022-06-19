910b5ee4950295db2208e03696098e65f8571f00
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-06-19 12:14:12 +0530

    profile

--- api/driver/driver.controller.js
+++ api/driver/driver.controller.js
@@ -4,12 +4,12 @@ const{createUser,getUserByMobile}=require('../user/user.controller');
 module.exports={
     startTrip:async(userId,bookingId,startKm)=>{
         treipRes=await startTrip(userId,bookingId,startKm);
-        console.log("treipRes==="+JSON.stringify(treipRes));
+        //console.log("treipRes==="+JSON.stringify(treipRes));
         return treipRes;
     },
     endTrip:async(userId,bookingId, endKm)=>{
         treipRes=await endTrip(userId,bookingId,endKm);
-        console.log("endTrip==="+JSON.stringify(treipRes));
+        //console.log("endTrip==="+JSON.stringify(treipRes));
         return treipRes;
     },
 }
