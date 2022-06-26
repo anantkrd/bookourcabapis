a8bd957daac6236c6956284d1c04bc90e3f2b1bf
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-06-26 23:41:40 +0530

    update api

--- api/booking/booking.router.js
+++ api/booking/booking.router.js
@@ -263,7 +263,7 @@ router.get('/getCabs',async function(req,res,next){
                     console.log("surgedestinationResult==="+JSON.stringify(surgedestinationResult));
                     if(cabTypecheck!=""){
                         surgePrice=surgekm*surgePickpuResult[0][cabType];
-                        surgePrice=surgePrice+surgekm*surgedestinationResult[0][cabType];
+                        surgePrice=surgePrice+(surgekm*surgedestinationResult[0][cabType]);
                         console.log("============surgePrice=========="+surgePrice);
                         finalRate=finalRate+surgePrice;
                         sedanPrice=finalRate;
