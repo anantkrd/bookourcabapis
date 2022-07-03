2f9074e622dc177e34f936aeee4275e4b575fed2
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-03 21:04:46 +0530

    update

--- api/admin/admin.service.js
+++ api/admin/admin.service.js
@@ -2,8 +2,8 @@ const pool=require('../../config/database');
 module.exports={
        
     getBookingsAdminHome:async(pageId,callBack)=>{
-        let start=((pageId-1)*10);
-        let perPage=10;
+        let start=((pageId-1)*5);
+        let perPage=5;
         sqlcheck="SELECT booking.*,cabs.cabType,cabs.ac,cabs.bags,cabs.capacity,cabs.cars,cabs.note,(select mobileNo from prayag_users where id=booking.userId ) as mobileNo FROM `prayag_booking` booking inner JOIN prayag_cabs cabs ON booking.cabId=cabs.id WHERE booking.isDeleted='N' and booking.status='waiting' order by booking.id desc limit ?,?";
         
         return new Promise((resolve, reject)=>{
