aacfbc24f4c99d94cfdfd55c08ed35f138893477
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-03 22:53:13 +0530

    update

--- api/admin/admin.service.js
+++ api/admin/admin.service.js
@@ -6,7 +6,7 @@ module.exports={
         let perPage=5;
         sqlcheck="SELECT booking.*,cabs.cabType,cabs.ac,cabs.bags,cabs.capacity,cabs.cars,cabs.note,(select mobileNo from prayag_users where id=booking.userId ) as mobileNo FROM `prayag_booking` booking inner JOIN prayag_cabs cabs ON booking.cabId=cabs.id WHERE booking.isDeleted='N' and booking.status='waiting' order by booking.id desc limit ?,?";
         sqlcheckCount="SELECT count(id) as rowCount FROM `prayag_booking` booking inner JOIN prayag_cabs cabs ON booking.cabId=cabs.id WHERE booking.isDeleted='N' and booking.status='waiting' order by booking.id desc";
-        let resCount=await getPageCount(sqlcheckCount,perPage);
+        let resCount=await this.getPageCount(sqlcheckCount,perPage);
 
         console.log("resCount=="+resCount);
         return new Promise((resolve, reject)=>{
