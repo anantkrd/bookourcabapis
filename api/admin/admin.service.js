be2f1977f309b26ab224b3afa23cc874f1fcab6a
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-03 22:46:19 +0530

    update

--- api/admin/admin.service.js
+++ api/admin/admin.service.js
@@ -46,8 +46,9 @@ module.exports={
         let start=((pageId-1)*10);
         let perPage=10;
         sqlcheck="SELECT booking.*,cabs.cabType,cabs.ac,cabs.bags,cabs.capacity,cabs.cars,cabs.note,(select mobileNo from prayag_users where id=booking.userId ) as mobileNo FROM `prayag_booking` booking inner JOIN prayag_cabs cabs ON booking.cabId=cabs.id WHERE booking.isDeleted='N' and driverId>0  and agentId>0 and booking.status='completed' order by booking.id desc limit ?,?";
-        sqlcheckCount="SELECT booking.*,cabs.cabType,cabs.ac,cabs.bags,cabs.capacity,cabs.cars,cabs.note,(select mobileNo from prayag_users where id=booking.userId ) as mobileNo FROM `prayag_booking` booking inner JOIN prayag_cabs cabs ON booking.cabId=cabs.id WHERE booking.isDeleted='N' and driverId>0  and agentId>0 and booking.status='completed' order by booking.id desc";
+        sqlcheckCount="SELECT count(*) as rowCount FROM `prayag_booking` booking inner JOIN prayag_cabs cabs ON booking.cabId=cabs.id WHERE booking.isDeleted='N' and driverId>0  and agentId>0 and booking.status='completed' order by booking.id desc";
         let resCount=await getPageCount(sqlcheckCount,perPage);
+
         console.log("resCount=="+resCount);
         return new Promise((resolve, reject)=>{
             pool.query(sqlcheck,[start,perPage],  (error, results)=>{
