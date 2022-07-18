9349756fc0142c849df1748ec1a87e47ce741449
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-18 21:19:53 +0530

    agent

--- api/admin/admin.router.js
+++ api/admin/admin.router.js
@@ -145,7 +145,20 @@ router.get('/update_agent_amount',async function(req,res,next){
     res.send(results);
 });  
 
-router.get('/get_agent',async function(req,res,next){
+router.get('/get_agent',authenticate,async function(req,res,next){
+    results =await getAgents(req.query.userId);
+    
+    if(results.length<=0){
+        responce=JSON.stringify({code:'500',msg:'No Data found',data:''});
+    }else{
+        
+        responce=JSON.stringify({code:'200',msg:'',data:results});
+    }
+     //return responce; 
+    res.send(responce);
+}); 
+
+router.get('/assign_direct_trip',authenticate,async function(req,res,next){
     results =await getAgents(req.query.userId);
     
     if(results.length<=0){
