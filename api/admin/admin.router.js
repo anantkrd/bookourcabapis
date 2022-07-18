4a4a3e12577b1959a6836452bcbb5e1487e9b5ca
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2022-07-18 21:33:21 +0530

    agent

--- api/admin/admin.router.js
+++ api/admin/admin.router.js
@@ -145,8 +145,18 @@ router.get('/update_agent_amount',async function(req,res,next){
     results =await updateAgentAmount(req.query.amount,req.query.bookingId);
     res.send(results);
 });  
-router.get('/get_agent',authenticate, async function(req,res,next){
+router.get('/get_agent',authenticate, async function(req, res, next) {
     results =await getAgents(req.query.userId);
+    if(result.length<=0){
+        responce=JSON.stringify({code:'501',message:'user not found',data:''});
+    }else{
+        responce=JSON.stringify({code:'200',message:'',data:result});
+    }
+    res.send(responce);
+    
+  });
+router.get('/get_agent2',authenticate, async function(req,res,next){
+   
     
     if(results.length<=0){
         responce=JSON.stringify({code:'500',msg:'No Data found',data:''});
